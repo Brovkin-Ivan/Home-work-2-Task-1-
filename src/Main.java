1a40cc01046d17a8b5402be0fc320b17ef743703
Brovkin Ivan <isbrovkinn@gmail.com>
2023-02-28 13:01:17 +0300

    Main

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13586;
+        int x = 20;
+
+        System.out.println((amount / x) + " Коллисество бонусных миль");
+    }
+}
